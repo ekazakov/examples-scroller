1a9941328a3a789fa23b55222923f53e48398b6f
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2016-02-29 00:25:52 +0300

    added infinity scroll

--- src/Scrollable.js
+++ src/Scrollable.js
@@ -5,26 +5,46 @@ export class Scrollable extends React.Component {
         super(...args);
         this.state = {
             viewportHeight: window.innerHeight,
-            scrollTop: window.pageYOffset,
-            offsetTopIndex: 0
+            scrollTop: window.pageYOffset
         };
 
         this.onScroll = this.onScroll.bind(this);
     }
 
-    onScroll() {}
+    onScroll() {
+        this.setState({scrollTop: window.pageYOffset});
+    }
 
     componentDidMount() {
         window.addEventListener('scroll', this.onScroll);
     }
 
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll);
+    }
+
     render() {
-        const from = this.state.offsetTopIndex;
-        const to = this.state.viewportHeight / this.props.rowHeight;
+        const {floor, min, ceil} = Math;
+        const {viewportHeight, scrollTop} = this.state;
+        const {size, rowHeight, children} = this.props;
+
+        const from = min(floor(scrollTop / rowHeight), 0);
+        const to = min(ceil((scrollTop + viewportHeight)/rowHeight), size);
+
         return <div>
+            <div style={{height: this._topPlaceholderHeight(from)}}></div>
             {React.Children.map(
-                this.props.children,
+                children,
                 (child) => React.cloneElement(child, {from, to}, child.props.children))}
+            <div style={{height: this._bottomPlaceholderHeight(size, to)}}></div>
         </div>;
+    }
+
+    _topPlaceholderHeight(from) {
+        return from * this.props.rowHeight;
+    }
+
+    _bottomPlaceholderHeight(size, to) {
+        return (size - to) * this.props.rowHeight;
     }
 }
